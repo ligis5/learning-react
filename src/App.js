ffa531733b775bec07b482149c247d15a4a95f1a
unknown <lukasnor5@gmail.com>
2021-01-12 10:49:38 +0200

    adding header component and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage.component';
-import {Route, Switch} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header-component'
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+
 
 function App() {
   return (
     <div>
+    <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -3,7 +3,9 @@ import {withRouter} from 'react-router-dom';
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) =>{
+    console.log(match)
+    return (
 <div style={{ backgroundImage:`url(${imageUrl})`}}
                 className={`${size} menu-item`}
                 onClick={() => history.push(`${match.url}${linkUrl}`)}
@@ -16,6 +18,6 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
                     <span className='subtitle'>SHOP NOW</span>
                 </div>
             </div>
-);
+)};
 
 export default withRouter (MenuItem);
